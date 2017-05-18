faf1e64cd40e73a2db4fd4b94f78044548fc604a
wnowak <wnowak10@gmail.com>
2017-05-18 12:34:02 -0400

    changed so that key is called svkey for streetview. limit ambiguity.

--- gimage.py
+++ gimage.py
@@ -1,10 +1,12 @@
 import streetview
+import secrets
+
 panoids = streetview.panoids(lat=-33.85693857571269, lon=151.2144895142714)
 
 direc = './' # where to put image. put in current working dir
 panoid = panoids[0]['panoid'] # get first image from this location
 heading = 0 # see docs for info on this. controls which direction we're looking
 flat_dir = direc
-key = secrets.key
+key = secrets.svkey
 
 r = streetview.api_download(panoid, heading, flat_dir, key)
